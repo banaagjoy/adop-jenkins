bed68beb8b71b6a2fdfdcc421abfaf50eccc61e4
Ivanov, Mihail <mihail.ivanov@accenture.com>
2017-01-18 15:03:29 +0000

    Modifying set_credentials to add users with ssh keys

--- resources/init.groovy.d/set_credentials.groovy
+++ resources/init.groovy.d/set_credentials.groovy
@@ -1,11 +1,12 @@
-import hudson.model.*
-import jenkins.model.*
-import hudson.security.*
-import jenkins.security.plugins.ldap.*
-import hudson.util.Secret
-import com.cloudbees.plugins.credentials.SystemCredentialsProvider
-import com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl
+import com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey;
 import com.cloudbees.plugins.credentials.CredentialsScope
+import com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl
+import com.cloudbees.plugins.credentials.SystemCredentialsProvider
+import hudson.model.*
+import hudson.security.*
+import hudson.util.Secret
+import jenkins.model.*
+import jenkins.security.plugins.ldap.*
 
 /**
  * @author Mihail Ivanov <mihail.ivanov@accenture.com>
@@ -19,30 +20,40 @@ import com.cloudbees.plugins.credentials.CredentialsScope
  *  - CREDENTIALS_SONAR_USER
  *  - CREDENTIALS_SONAR_USER_PASSWORD
  *  - CREDENTIALS_SONAR_USER_DESCRIPTION
+ *  - CREDENTIALS_JENKINSSLAVE_KEY_ID
+ *  - CREDENTIALS_JENKINSSLAVE_KEY_USERNAME
+ *  - CREDENTIALS_JENKINSSLAVE_KEY_DESCRIPTION
+ *  - CREDENTIALS_JENKINSSLAVE_KEY_PATH (leave blank to use ~/.ssh)
+ *  - CREDENTIALS_APPUSER_KEY_ID
+ *  - CREDENTIALS_APPUSER_KEY_USERNAME
+ *  - CREDENTIALS_APPUSER_KEY_DESCRIPTION
+ *  - CREDENTIALS_APPUSER_KEY_PATH (leave blank to use ~/.ssh)
  */
 
 // fetch Jenkins state
 def env = System.getenv()
 def instance = Jenkins.getInstance()
 
-
 Thread.start {
 	// executing the following lines as admin user instead of anonymous
 	ACL.impersonate(User.get(env['INITIAL_ADMIN_USER']).impersonate())
-	println 'User: ' + User.current()
+	println 'Executing script as user: ' + User.current()
 
     println '--> Configuring global credentials'
    	// define credentials
-   	// parameters (credential_id, credential_username, credential_password, credential_description)
+   	// parameters - credential_id, credential_username, credential_password, credential_description
 	def ldap_service_user = new Tuple(env['CREDENTIALS_LDAP_SERVICE_USER_ID'], env['CREDENTIALS_LDAP_SERVICE_USER'], env['CREDENTIALS_LDAP_SERVICE_USER_PASSWORD'], env['CREDENTIALS_LDAP_SERVICE_DESCRIPTION'])
 	def sonar_user = new Tuple(env['CREDENTIALS_SONAR_USER_ID'], env['CREDENTIALS_SONAR_USER'], env['CREDENTIALS_SONAR_USER_PASSWORD'], env['CREDENTIALS_SONAR_USER_DESCRIPTION'])
 
-	// TODO(mihail): jenkinsslave + private key
+	// parameters - ssh_key_id, ssh_key_username, ssh_key_description, private_key_path
+	def jenkinsslave_user = new Tuple(env['CREDENTIALS_JENKINSSLAVE_KEY_ID'], env['CREDENTIALS_JENKINSSLAVE_KEY_USERNAME'], env['CREDENTIALS_JENKINSSLAVE_KEY_DESCRIPTION'], env['CREDENTIALS_JENKINSSLAVE_KEY_PATH'])
+	def appuser = new Tuple(env['CREDENTIALS_APPUSER_KEY_ID'], env['CREDENTIALS_APPUSER_KEY_USERNAME'], env['CREDENTIALS_APPUSER_KEY_DESCRIPTION'], env['CREDENTIALS_APPUSER_KEY_PATH'])
+
     // define two lists with all the credentials
     user_with_password_list = [sonar_user, ldap_service_user]
+    user_with_ssh_key_list = [jenkinsslave_user, appuser]
 
-	// http://javadoc.jenkins-ci.org/credentials/com/cloudbees/plugins/credentials/SystemCredentialsProvider.html
-
+	// set up username + password
 	user_with_password_list.each {
 	    user_with_password = (Tuple) it
 	    println "--> Registering credentials"
@@ -69,6 +80,45 @@ Thread.start {
 	        def credential_creds = new UsernamePasswordCredentialsImpl(credential_scope,credential_id,credential_description,credential_username,credential_password)
 
 	        system_credentials_provider.addCredentials(credential_domain,credential_creds)
+	        println "--> Added credentials for user: " + credential_id
+	    }
+	}
+
+	// set up username + private key
+	user_with_ssh_key_list.each {
+		user_with_ssh_key = (Tuple) it
+	    println "--> Registering SSH Credentials"
+	    def system_credentials_provider = SystemCredentialsProvider.getInstance()
+
+	    def ssh_key_description = user_with_ssh_key.get(2)
+
+	    ssh_credentials_exist = false
+	    system_credentials_provider.getCredentials().each {
+	        credentials = (com.cloudbees.plugins.credentials.Credentials) it
+	        if ( credentials.getDescription() == ssh_key_description) {
+	            ssh_credentials_exist = true
+	            println("Found existing credentials: " + ssh_key_description)
+	        }
+	    }
+
+	    if(!ssh_credentials_exist) {
+	        def ssh_key_scope = CredentialsScope.GLOBAL
+	        def ssh_key_id = user_with_ssh_key.get(0)
+	        def ssh_key_username = user_with_ssh_key.get(1)
+
+	        // use the keys in ~/.ssh
+	        def ssh_key_private_key_source = new BasicSSHUserPrivateKey.UsersPrivateKeySource()
+
+	        // otherwise use path if specified 
+	        if (user_with_ssh_key.get(3).size() > 0) {
+	        	ssh_key_private_key_source = new BasicSSHUserPrivateKey.FileOnMasterPrivateKeySource(user_with_ssh_key.get(3))
+	        }
+	        def ssh_key_passphrase = null
+	        def ssh_key_domain = com.cloudbees.plugins.credentials.domains.Domain.global()
+	        def ssh_key_creds = new BasicSSHUserPrivateKey(ssh_key_scope,ssh_key_id,ssh_key_username,ssh_key_private_key_source,ssh_key_passphrase,ssh_key_description)
+
+	        system_credentials_provider.addCredentials(ssh_key_domain,ssh_key_creds)
+	        println "--> Added credentials for user: " + ssh_key_username
 	    }
 	}
 
